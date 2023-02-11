ea82d2c288e45758eaad78217b9ab4de756e9fdb
Agus SFD <124112690+agussaifuddin@users.noreply.github.com>
2023-02-12 02:22:03 +0700

    Update MyWalletAddress.sol

--- MyWalletAddress/MyWalletAddress.sol
+++ MyWalletAddress/MyWalletAddress.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol";
 
-contract GameItems is ERC1155 {
+contract MyWalletAddress is ERC1155 {
     uint256 public constant BTC = 0;
     uint256 public constant ETH = 1;
     uint256 public constant BNB = 2;
