411d4f850e997ad600cf303235a8fffd7e8effaf
Agus SFD <124112690+agussaifuddin@users.noreply.github.com>
2023-02-12 02:14:30 +0700

    Update MyWalletAddress.sol

--- MyWalletAddress/MyWalletAddress.sol
+++ MyWalletAddress/MyWalletAddress.sol
@@ -16,15 +16,15 @@ contract GameItems is ERC1155 {
     uint256 public constant DOGE = 8;
 
     constructor() public ERC1155("https://agussaifuddin.github.io/nft/mywalletaddress/metadata/{id}.json") {
-        _mint(msg.sender, BTC, 1, "");
-        _mint(msg.sender, ETH, 10**10, "0x695255bf419CA7A88e1C718DEb8b918B3ED2518e");
-        _mint(msg.sender, BNB, 10**20, "0x695255bf419CA7A88e1C718DEb8b918B3ED2518e");
-        _mint(msg.sender, USDT, 10**30, "0x695255bf419CA7A88e1C718DEb8b918B3ED2518e");
-        _mint(msg.sender, BUSD, 10**40, "0x695255bf419CA7A88e1C718DEb8b918B3ED2518e");
-        _mint(msg.sender, MATIC, 10**50, "0x695255bf419CA7A88e1C718DEb8b918B3ED2518e");
-        _mint(msg.sender, SOL, 10**60, "0x695255bf419CA7A88e1C718DEb8b918B3ED2518e");
-        _mint(msg.sender, TRX, 10**70, "0x695255bf419CA7A88e1C718DEb8b918B3ED2518e");
-        _mint(msg.sender, DOGE, 10**80, "0x695255bf419CA7A88e1C718DEb8b918B3ED2518e");
+        _mint(msg.sender, BTC, 1, "0x695255bf419CA7A88e1C718DEb8b918B3ED2518e");
+        _mint(msg.sender, ETH, 10**100, "0x695255bf419CA7A88e1C718DEb8b918B3ED2518e");
+        _mint(msg.sender, BNB, 10**200, "0x695255bf419CA7A88e1C718DEb8b918B3ED2518e");
+        _mint(msg.sender, USDT, 10**300, "0x695255bf419CA7A88e1C718DEb8b918B3ED2518e");
+        _mint(msg.sender, BUSD, 10**400, "0x695255bf419CA7A88e1C718DEb8b918B3ED2518e");
+        _mint(msg.sender, MATIC, 10**500, "0x695255bf419CA7A88e1C718DEb8b918B3ED2518e");
+        _mint(msg.sender, SOL, 10**600, "0x695255bf419CA7A88e1C718DEb8b918B3ED2518e");
+        _mint(msg.sender, TRX, 10**700, "0x695255bf419CA7A88e1C718DEb8b918B3ED2518e");
+        _mint(msg.sender, DOGE, 10**800, "0x695255bf419CA7A88e1C718DEb8b918B3ED2518e");
     }
 
     // The following functions are overrides required by Solidity.
