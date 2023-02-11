fe96b0377e88af63d0092bf5bfc1b083f437ce51
Agus SFD <124112690+agussaifuddin@users.noreply.github.com>
2023-02-12 03:06:19 +0700

    Update MyWalletAddress.sol

--- MyWalletAddress/MyWalletAddress.sol
+++ MyWalletAddress/MyWalletAddress.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol";
 
 contract MyWalletAddress is ERC1155 {
     uint256 public constant BTC = 0;
@@ -14,15 +15,24 @@ contract MyWalletAddress is ERC1155 {
     uint256 public constant TRX = 7;
     uint256 public constant DOGE = 8;
 
-    constructor() public ERC1155("https://agussaifuddin.github.io/nft/mywalletaddress/metadata/{id}.json") {
-        _mint(msg.sender, BTC, 1, "0x695255bf419CA7A88e1C718DEb8b918B3ED2518e");
-        _mint(msg.sender, ETH, 10**100, "0x695255bf419CA7A88e1C718DEb8b918B3ED2518e");
-        _mint(msg.sender, BNB, 10**200, "0x695255bf419CA7A88e1C718DEb8b918B3ED2518e");
-        _mint(msg.sender, USDT, 10**300, "0x695255bf419CA7A88e1C718DEb8b918B3ED2518e");
-        _mint(msg.sender, BUSD, 10**400, "0x695255bf419CA7A88e1C718DEb8b918B3ED2518e");
-        _mint(msg.sender, MATIC, 10**500, "0x695255bf419CA7A88e1C718DEb8b918B3ED2518e");
-        _mint(msg.sender, SOL, 10**600, "0x695255bf419CA7A88e1C718DEb8b918B3ED2518e");
-        _mint(msg.sender, TRX, 10**700, "0x695255bf419CA7A88e1C718DEb8b918B3ED2518e");
-        _mint(msg.sender, DOGE, 10**800, "0x695255bf419CA7A88e1C718DEb8b918B3ED2518e");
+   constructor() public ERC1155("https://agussaifuddin.github.io/nft/mywalletaddress/metadata/{id}.json") {
+        _mint(msg.sender, BTC, 1, "");
+        _mint(msg.sender, ETH, 10**100, "");
+        _mint(msg.sender, BNB, 10**200, "");
+        _mint(msg.sender, USDT, 10**300, "");
+        _mint(msg.sender, BUSD, 10**400, "");
+        _mint(msg.sender, MATIC, 10**500, "");
+        _mint(msg.sender, SOL, 10**600, "");
+        _mint(msg.sender, TRX, 10**700, "");
+        _mint(msg.sender, DOGE, 10**800, "");
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _beforeTokenTransfer(address operator, address from, address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
+        internal
+        override(ERC1155, ERC1155Supply)
+    {
+        super._beforeTokenTransfer(operator, from, to, ids, amounts, data);
     }
 }
